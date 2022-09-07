e52387c8ebe590d4f07d92dfa03069e5073b4809
Ariful Islam Polash <arifulislampolash.25.fswad@gmail.com>
2022-09-07 16:25:04 +0600

    Route work is done

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
-
+use App\Events\Message;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::post('send-message', function (Request $request){
+    event(new Message($request->username, $request->message));
+    return ['success' => true];
 });
