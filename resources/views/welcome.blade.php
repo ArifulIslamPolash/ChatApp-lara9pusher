383c354217ddb2feaf13329494d1680815097a96
Ariful Islam Polash <arifulislampolash.25.fswad@gmail.com>
2022-09-09 21:48:40 +0600

    vite configuration and css added design done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,12 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+
     <title>Chat App</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
   </head>
   <body>
     <h1>RealTime Chat App</h1>
